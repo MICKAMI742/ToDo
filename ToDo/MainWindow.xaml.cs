bd51b141bfb40c9a21db409dd9d7a249cb16deb1
Michał Kamiński <MICKAMI52@edu.gdansk.pl>
2022-11-28 15:26:10 +0100

    Version 1.2

--- ToDo/MainWindow.xaml.cs
+++ ToDo/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 using ToDo.Pages;
 
 namespace ToDo
@@ -21,9 +22,39 @@ namespace ToDo
     /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer timer;
+        double panelWidth;
+        bool hidden;
         public MainWindow()
         {
             InitializeComponent();
+            timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0,0,0,0,10);
+            timer.Tick += Timer_Tick;
+
+            panelWidth = slidePanel.Width;
+        }
+
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            if (hidden)
+            {
+                slidePanel.Width += 1;
+                if (slidePanel.Width >= panelWidth)
+                {
+                    timer.Stop();
+                    hidden = false;
+                }
+            }
+            else
+            {
+                slidePanel.Width -= 1;
+                if (slidePanel.Width <= 40)
+                {
+                    timer.Stop();
+                    hidden = true;
+                }
+            }
         }
 
         private void ButtonTasks_Click(object sender, RoutedEventArgs e)
@@ -34,6 +65,19 @@ namespace ToDo
         private void ButtonAdd_Click(object sender, RoutedEventArgs e)
         {
             Main.Content = new AddPage();
+        }
+
+        private void ButtonMenu_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Start();
+        }
+
+        private void PanelHeader_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
         }
     }
 }
